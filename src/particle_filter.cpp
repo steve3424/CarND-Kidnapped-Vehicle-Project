17606800483386f4c8e2c800c49b69eeb086d256
Steve <stevefrazee123@gmail.com>
2018-10-30 19:43:27 -0400

    add init function

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -24,7 +24,33 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
+	
+	// create normal distribution for sampling
+	default_random_engine gen;
+	normal_distribution<double> dist_x(x, std[0]);
+	normal_distribution<double> dist_y(y, std[1]);
+	normal_distribution<double> dist_theta(theta, std[2]);
 
+	// set num of particles
+	num_particles = 1000;
+
+	// create all particles
+	for (int i=0; i < num_particles; ++i) {
+		// create single particle
+		Particle particle;	
+		particle.id = i;
+		particle.x = dist_x(gen);
+		particle.y = dist_y(gen);
+		particle.theta = dist_theta(gen);
+		particle.weight = 1;
+
+		particles.push_back(particle);
+
+		// init weight vector
+		weights.push_back(1);		
+	}
+	
+	is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
